e1d097fd11e7e6d5d8e8b606a41bab34bf689ed2
Joël Pittet <pittet@cs.ubc.ca>
2024-11-28 10:46:18 -0800

    Excute all commands within the previous directory and fix destination to be the created config/backup directory

--- recipes/drupal8.php
+++ recipes/drupal8.php
@@ -23,23 +23,23 @@ task('deploy:drush', function () {
 
 desc('Execute drush config:export to backup previous config');
 task('drush:config:backup', function() {
-    // Skip when there aren't database connection variables.
-    if (! test('[ -s {{release_or_current_path}}/.env ]')) {
-        writeln("<fg=yellow;options=bold;>Warning: </><fg=yellow;>Your .env file is empty! Skipping...</>");
-        return;
-    }
-
-    // Skip when there are no tables in the database.
-    if (test('[[ -z "$({{release_or_current_path}}/vendor/bin/drush sql:query \'SHOW TABLES\')" ]]')) {
-        writeln("<fg=yellow;options=bold;>Warning: </><fg=yellow;>Your database is empty! Skipping...</>");
-        return;
-    }
-
     $destination = has('previous_release') ? '{{previous_release}}' : '{{release_path}}';
-    run("mkdir -p $destination/config/backup");
+    // Execute all within the previous release or current release directory.
+    within($destination, function () use ($destination) {
+        // Skip when there aren't database connection variables.
+        if (! test('[ -s ./.env ]')) {
+            writeln("<fg=yellow;options=bold;>Warning: </><fg=yellow;>Your .env file is empty! Skipping...</>");
+            return;
+        }
 
-    cd('{{release_or_current_path}}');
-    run("{{release_or_current_path}}/vendor/bin/drush -y config:export --destination=$destination");
-    writeln('Backup saved to ' . $destination);
+        // Skip when there are no tables in the database.
+        if (test('[[ -z "$(./vendor/bin/drush sql:query \'SHOW TABLES\')" ]]')) {
+            writeln("<fg=yellow;options=bold;>Warning: </><fg=yellow;>Your database is empty! Skipping...</>");
+            return;
+        }
+        run("mkdir -p ./config/backup");
+        run("./vendor/bin/drush -y config:export --destination=$destination/config/backup");
+        writeln("Backup saved to $destination/config/backup");
+    });
 });
 before('deploy:drush', 'drush:config:backup');
